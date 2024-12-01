5715c533f51894b9fd58c5d1f00b98fc14eb30ca
Jake Champion <me@jakechampion.name>
2024-12-01 12:45:03 +0000

    Update main.rs

--- netlify/functions/hello-world/src/main.rs
+++ netlify/functions/hello-world/src/main.rs
@@ -1,35 +1,30 @@
-use aws_lambda_events::encodings::Body;
-use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
-use http::header::HeaderMap;
-use lambda_runtime::{handler_fn, Context, Error};
-use log::LevelFilter;
-use simple_logger::SimpleLogger;
+use rand::seq::SliceRandom;
+use serde_json::json;
+use simple_runtime_demo::choose_starter;
+use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    SimpleLogger::new()
-        .with_level(LevelFilter::Info)
-        .init()
-        .unwrap();
-
-    let func = handler_fn(my_handler);
-    lambda_runtime::run(func).await?;
-    Ok(())
+    run(handler).await
 }
 
-pub(crate) async fn my_handler(
-    event: ApiGatewayProxyRequest,
-    _ctx: Context,
-) -> Result<ApiGatewayProxyResponse, Error> {
-    let path = event.path.unwrap();
+async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    let starter = choose_starter();
 
-    let resp = ApiGatewayProxyResponse {
-        status_code: 200,
-        headers: HeaderMap::new(),
-        multi_value_headers: HeaderMap::new(),
-        body: Some(Body::Text(format!("🦀 Hello, World 🦀 from '{}'", path))),
-        is_base64_encoded: Some(false),
-    };
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(
+            json!({
+              "message": format!("I choose you, {}!", starter),
+            })
+            .to_string()
+            .into(),
+        )?)
+}
 
-    Ok(resp)
+fn choose_starter() -> String {
+    let pokemons = vec!["Bulbasaur", "Charmander", "Squirtle", "Pikachu"];
+    let starter = pokemons.choose(&mut rand::thread_rng()).unwrap();
+    starter.to_string()
 }
